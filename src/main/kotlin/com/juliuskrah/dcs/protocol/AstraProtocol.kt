1fd7d5485ed96e780a3836194c19e9f52896fed9
Julius Krah <juliuskrah@gmail.com>
2020-04-24 22:20:26 +0000

    implementation of 'M' protocol done

--- src/main/kotlin/com/juliuskrah/dcs/protocol/AstraProtocol.kt
+++ src/main/kotlin/com/juliuskrah/dcs/protocol/AstraProtocol.kt
@@ -6,7 +6,12 @@ import io.netty.buffer.ByteBuf
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import reactor.core.publisher.Mono
+import java.lang.String.format
 import java.nio.charset.Charset
+import java.nio.charset.StandardCharsets.UTF_8
+import java.time.Duration
+import java.time.Instant.ofEpochSecond
+import java.time.ZoneOffset.UTC
 
 /**
  * The Astra protocol
@@ -54,8 +59,15 @@ open class AstraProtocol : Protocol {
      * Handles binary implementation for the astra protocol
      */
     abstract class BinaryProtocol : AstraProtocol() {
+        protected var statusReportsToFollow = 0x10
+        protected var statusExtraData = 0x100
+        private var dbmMask = 0xF0
+        protected var satMask = 0x0F
+        protected var reasonJourneyStart = 0x40
+        protected var reasonJourneyStop = 0x80
+
         /**
-         *  Lookup table required for checksum generation
+         *  CRC16 lookup table required for checksum generation
          */
         private val laCrc16LookUpTable = intArrayOf(0x0000, 0xC0C1, 0xC181, 0x0140, 0xC301, 0x03C0, 0x0280, 0xC241,
                 0xC601, 0x06C0, 0x0780, 0xC741, 0x0500, 0xC5C1, 0xC481, 0x0440, 0xCC01, 0x0CC0, 0x0D80, 0xCD41, 0x0F00,
@@ -100,6 +112,49 @@ open class AstraProtocol : Protocol {
             return llcrc
         }
 
+        /**
+         * Determine signal strength from signal quality
+         */
+        protected fun @ExtensionFunctionType Int.determineSignalStrength(): Int {
+            val signalQuality = this
+            val highNibble = (signalQuality and dbmMask) shr 4
+
+            // Possible dbm values according to protocols
+            val dbm = intArrayOf(-111, -109, -105, -101, -97, -93, -89, -85, -81, -77, -73, -69, -65, -61, -57, -63)
+            //If the reported signal level is in range, returns dBm info
+            return if (highNibble in 0..15) {
+                dbm[highNibble]
+            } else {
+                1
+            }
+        }
+
+        fun @ExtensionFunctionType Int.toHexString(bitLength: Int): String {
+            return toHexString(this.toLong() and 0xFFFFFFFFL, bitLength)
+        }
+
+        protected fun toHexString(`val`: Long, bitLength: Int): String {
+            // bounds check 'bitLen'
+            var bitLen = bitLength
+            if (bitLen <= 0) {
+                bitLen = 64 - java.lang.Long.numberOfLeadingZeros(`val`)
+                if (bitLen <= 0) {
+                    bitLen = 8
+                }
+            } else if (bitLen > 64) {
+                bitLen = 64
+            }
+
+            // format and return hex value
+            val nybbleLen = (bitLen + 7) / 8 * 2
+            val hex = StringBuffer(java.lang.Long.toHexString(`val`).toUpperCase())
+            if (nybbleLen <= 16 && nybbleLen > hex.length) {
+                val mask = "0000000000000000" // 64 bit (16 nybbles)
+                hex.insert(0, mask.substring(0, nybbleLen - hex.length))
+            }
+            return hex.toString()
+        }
+
         override fun processing(byteBuf: ByteBuf): Any {
             val bytes: ByteArray
             val length = byteBuf.readableBytes()
@@ -124,10 +179,157 @@ open class AstraProtocol : Protocol {
             private val statusIgnitionOn = 0x01
             private val protocolMBasicLength = 41
             private val protocolMStartStopLength = 53
+            private val commands = mapOf(
+                    1 to "\$TEST,1",
+                    2 to "\$PARA,1",
+                    3 to "\$POLL",
+                    4 to "\$POSN,k,20",
+                    5 to "\$IMEI"
+            )
 
             override fun parseProtocol(byteBuf: ByteBuf): ByteArray {
                 log.info("Parsing protocol M...")
-                return byteArrayOf(0x07) // TODO return 0x06
+                byteBuf.readUnsignedShort() // packet length
+                val imei = format("%08d", byteBuf.readUnsignedInt()) + // tac
+                        format("%07d", byteBuf.readUnsignedMedium())  // msn
+                var reportsToFollow: Boolean
+                // May contain more than one report after parsing the headers
+                do {
+                    byteBuf.readUnsignedByte() // sequence number
+                    val latitude = (byteBuf.readInt() / 1000000f).toDouble()
+                    val longitude = (byteBuf.readInt() / 1000000f).toDouble()
+                    val fixTime: Long = byteBuf.readUnsignedInt() + 315964800L
+                    val instant = ofEpochSecond(fixTime)
+                    // Read Julian time - time and date in GPS seconds and convert to
+                    // seconds from Unix epoch by adding the difference between
+                    // 00:00:00 6 January 1980 and 00:00:00 1 January 1970
+                    // The stored time will be GMT
+                    val time = instant.atZone(UTC)
+                    // Convert speed stored as km/h divided by 2 to km/h
+                    val speed = (byteBuf.readUnsignedByte() * 2F).toDouble()
+                    // Convert heading stored as degrees divided by 2 to degrees
+                    val heading = (byteBuf.readUnsignedByte() * 2F).toDouble()
+                    val reportReason = byteBuf.readUnsignedMedium()
+                    val reportStatus = byteBuf.readUnsignedShort()
+                    // Digital input/output status and state changes
+                    // Read as little-endian so that Digitals #1 is the least significant byte
+                    val digitalInputs = byteBuf.readMediumLE().toLong()
+                    // ADC1 0-5V - convert to voltage with resolution of 0.02V
+                    val adc1 = (byteBuf.readUnsignedByte() * 20).toDouble() / 1000F
+                    // ADC2 0-15V - convert to voltage with resolution of 0.059V
+                    val adc2 = (byteBuf.readUnsignedByte() * 59).toDouble() / 1000F
+                    // Battery level as a percentage
+                    val batteryLevel = byteBuf.readUnsignedByte().toInt()
+                    // External input voltage to 0.2V resolution - convert to actual voltage
+                    val externalPower = byteBuf.readUnsignedByte().toDouble() / 5f
+                    // Convert max speed stored as km/h divided by 2 to km/h
+                    // During a journey this is max speed since last report
+                    // At the end of a journey it is max speed during the entire journey
+                    val maxSpeed = byteBuf.readUnsignedByte() * 2
+                    val accelerometerData = IntArray(6)
+                    for (i in 0..5) {
+                        accelerometerData[i] = byteBuf.readUnsignedByte().toInt()
+                    }
+                    // Journey distance to 0.1km resolution - convert to actual distance
+                    val journeyDistance = byteBuf.readUnsignedShort().toDouble() / 10f
+                    // Time in seconds that the vehicle is stationary with the ignition on
+                    val journeyIdleTime = byteBuf.readUnsignedShort()
+                    // Altitude in metres divided by 20 - convert to meters
+                    val altitude = byteBuf.readUnsignedByte().toDouble() * 20f
+                    // Most significant nibble is GSM signal strength: scale 0-15
+                    // Least significant nibble is the number of GPS satellites in use
+                    val signalQuality = byteBuf.readUnsignedByte().toInt()
+                    val signalStrength = signalQuality.determineSignalStrength()
+                    val satelliteCount = signalQuality and satMask
+                    // Geofence: Bit 7=1 for entry, Bit 7=0 for exit. Bits 6-0=geofence index
+                    val geoFence: Int = byteBuf.readUnsignedByte().toInt()
+                    var odometer: Double
+                    var engineOnHours: Long
+                    // Read Driver ID
+                    var driverUniqueId = "N/A"
+                    if (reportStatus and statusExtraData > 0) {
+                        val iButtonFN: Int = byteBuf.readUnsignedByte().toInt()
+                        val iButtonSN: String = byteBuf.readCharSequence(6, UTF_8).toString()
+                        driverUniqueId = StringBuilder() //
+                                .append(iButtonFN.toHexString(8)).append('-') //
+                                .append(iButtonSN).toString()
+                    }
+
+                    //If there is a journey start or stop, the device's odometer is read
+                    if (reportReason and reasonJourneyStart > 0 || reportReason and reasonJourneyStop > 0) {
+                        odometer = byteBuf.readUnsignedMedium().toDouble()
+                        engineOnHours = byteBuf.readUnsignedShort().toLong()
+                    } else {
+                        val lastDistanceKM = 0.0
+                        val lastOdometer = 0.0
+                        val lastEngineOnHours: Long = 0
+                        if (reportStatus and statusIgnitionOn > 0) {
+                            // Calculates odometer using the device's data
+                            odometer = lastOdometer - lastDistanceKM + journeyDistance
+                            // check if there is some way to calculate the difference
+                            // if not put the last value.
+                            engineOnHours = lastEngineOnHours
+                        } else { // If the device is not a journey or start/stop event, odometer doesn't change
+                            odometer = lastOdometer
+                            engineOnHours = lastEngineOnHours
+                        }
+                    }
+                    var reportLength: Int
+                    reportLength = if (reportStatus and statusExtraData > 0)
+                        protocolMStartStopLength
+                    else protocolMBasicLength
+                    // TODO status code and hex
+                    // var journeyStart = if (reportReason and reasonJourneyStart > 0) 1 else 0
+                    // var journeyStop = if (reportReason and reasonJourneyStop > 0) 1 else 0
+
+                    val sb = StringBuilder()
+                    val rawData: String = sb.append("R=").append(reportReason.toHexString(24)) //
+                            .append(";S=").append(reportStatus.toHexString(16)) //
+                            .append(";P=").append(externalPower).append('V') //
+                            .append(";B=").append(batteryLevel).append('%') //
+                            .append(";D=").append(toHexString(digitalInputs, 24)) //
+                            .append(";A1=").append(adc1).append('V') //
+                            .append(";A2=").append(adc2).append('V') //
+                            .append(";M=").append(maxSpeed).append("km/h") //
+                            .append(";X=").append(accelerometerData[0]).append(',').append(accelerometerData[1]) //
+                            .append(";Y=").append(accelerometerData[2]).append(',').append(accelerometerData[3]) //
+                            .append(";Z=").append(accelerometerData[4]).append(',').append(accelerometerData[5]) //
+                            .append(";I=").append(journeyIdleTime).append('s') //
+                            .append(";Q=").append(signalQuality.toHexString(8)) //
+                            .append(";G=").append(geoFence.toHexString(8)) //
+                            .toString()
+
+                    log.info("IMEI number: \t\t{}", imei)
+                    log.info("Report length: \t\t{}", reportLength)
+                    log.info("Latitude: \t\t\t{}", latitude)
+                    log.info("Longitude: \t\t{}", longitude)
+                    log.info("Event time: \t\t{}", time)
+                    log.info("Speed: \t\t\t{}km/h", speed)
+                    log.info("Max speed: \t\t{}km/h", maxSpeed)
+                    log.info("Heading: \t\t\t{}°", heading)
+                    log.info("ADC 1: \t\t\t{}V", adc1)
+                    log.info("ADC 2: \t\t\t{}V", adc2)
+                    log.info("Battery level: \t\t{}%", batteryLevel)
+                    log.info("External power: \t\t{}V", externalPower)
+                    log.info("Idle time: \t\t{}", Duration.ofSeconds(journeyIdleTime.toLong()))
+                    log.info("Altitude: \t\t\t{}m", altitude)
+                    log.info("Signal quality: \t\t{}", signalQuality)
+                    log.info("Signal strength: \t\t{}", signalStrength)
+                    log.info("Satellite count: \t\t{}", satelliteCount)
+                    log.info("Geofence: \t\t\t{}", geoFence)
+                    log.info("Odometer: \t\t\t{}km", odometer)
+                    log.info("Engine hours: \t\t{}h", engineOnHours)
+                    log.info("Driver ID: \t\t{}", driverUniqueId)
+                    log.info("Raw data: \t\t\t{}", rawData)
+
+                    // Check for more reports in the packet
+                    reportsToFollow = reportStatus and statusReportsToFollow > 0
+                } while (reportsToFollow && byteBuf.readableBytes() > 2)
+                val random = (1..5).random()
+                val command = commands[random] ?: throw Exception("No command available")
+                log.info("\n----------")
+                return command.toByteArray()
+                // return byteArrayOf(0x07) // TODO return 0x06
             }
         }
 
@@ -152,11 +354,21 @@ open class AstraProtocol : Protocol {
         abstract fun parseProtocol(command: String): String
 
         class AstraCommand : StringProtocol() {
+            private val commands = mapOf(
+                    1 to "\$TEST,1",
+                    2 to "\$PARA,1",
+                    3 to "\$POLL",
+                    4 to "\$POSN,k,20",
+                    5 to "\$IMEI"
+            )
             private val log: Logger = LoggerFactory.getLogger(AstraCommand::class.java)
             override fun parseProtocol(command: String): String {
                 log.info("Command: {}", command)
-                log.info("last line \n-------")
-                return "ByteArray(0x06)" // TODO return command
+                val random = (1..5).random()
+                val command = commands[random] ?: throw Exception("No command available")
+                log.info(command)
+                log.info("\n----------")
+                return "\$DRID,APPROVE,01,00000000125408C9" // TODO return drid command
             }
         }
     }
